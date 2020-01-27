d89e3a4dcf85c20102d96bf83bbd8a5c59e85acd
ScarVite <43887376+ScarVite@users.noreply.github.com>
2020-01-27 23:39:02 +0100

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("emailform").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("emailform").style.display = "none";
+}
